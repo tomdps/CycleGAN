408e572e50f1be61422b42985e42aaf6d164576b
Tom <t.dupuis@bcimediagroup.com>
2021-01-16 17:18:15 +0100

    corrected indent errors

--- train.py
+++ train.py
@@ -13,19 +13,19 @@ def train(cycle_gan, dataloader1, dataloader2, max_iter = 7000, loss_gan = error
     dl1_iter = iter(dataloader1)
     dl2_iter = iter(dataloader2)
     iter_per_epoch = min(len(dl1_iter), len(dl2_iter))
-      
+        
     for iteration in max_iter:
-      
-    	if (iteration +1) % iter_per_epoch == 0:
+        
+        if (iteration +1) % iter_per_epoch == 0:
             dl1_iter = iter(dataloader1)
             dl2_iter = iter(dataloader2)
             
-    	data_1 = dl1_iter.next().cuda()
-    	data_2 = dl1_iter.next().cuda()
-        
+        data_1 = dl1_iter.next().cuda()
+        data_2 = dl1_iter.next().cuda()
+
         cycle_gan.g_opt.zero_grad()
         cycle_gan.d_opt.zero_grad()
-        
+
         D1_real = D1(data_2)
         d1_loss_real = loss_gan(D1_real-1)
         D2_real = D2(data_1)
@@ -33,7 +33,7 @@ def train(cycle_gan, dataloader1, dataloader2, max_iter = 7000, loss_gan = error
         d_loss_real = d1_loss_real + d2_loss_real
         d_loss_real.backward()
         cycle_gan.d_opt.step()
-        
+
         D1_fake = D1(G1(data_1))
         d1_loss_fake = loss_gan(D1_fake)
         D2_fake = D2(G2(data_2))
@@ -44,7 +44,7 @@ def train(cycle_gan, dataloader1, dataloader2, max_iter = 7000, loss_gan = error
         
         cycle_gan.g_opt.zero_grad()
         cycle_gan.d_opt.zero_grad()
-		
+
         G1_gen = G1(data_1)
         D1_gen = D1(G1_gen)
         G2oG1_gen = G2(G1_gen)
@@ -66,6 +66,6 @@ def train(cycle_gan, dataloader1, dataloader2, max_iter = 7000, loss_gan = error
         
         if (iteration+1) % 100 == 0:
             print('Iter [%d/%d], d_loss_real: %.4f, d_loss_fake: %.4f, G1_loss: %.4f, '
-                  'G2_loss: %.4f'
-                  %(iteration+1, max_iter, d_loss_real.data[0], d_loss_fake.data[0], 
+                    'G2_loss: %.4f'
+                    %(iteration+1, max_iter, d_loss_real.data[0], d_loss_fake.data[0], 
                     G1_loss.data[0], G2_loss.data[0]))
